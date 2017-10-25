5052ad037d703b33b692760155be1dede073d8de
Andreas Lüdemann <Andreas@Andreas-MacBook-Pro.local>
2017-10-25 09:50:56 +0200

    Refactor

--- app.js
+++ app.js
@@ -1,29 +1,20 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const api = require('./app/routes');
+const app = express();
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
 
-app.use('/', index);
-app.use('/users', users);
+// remove when angular is added
+app.set('views', __dirname + '/app/views');
+app.set('view engine', 'jade');
+app.use('/', api); // delete when angular is added
+
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
